1c75da4083cdfad024863489403110f6fc17d8bd
ParthsBadgujar <2024.parth.badgujar@ves.ac.in>
2025-09-25 09:27:48 +0530

    Create file1.java

---
+++ file1.java
@@ -0,0 +1,10 @@
+public class SumExample {
+    public static void main(String[] args) {
+        int a = 5;  // You can change this value
+        int b = 7;  // You can change this value
+
+        int sum = a + b;
+
+        System.out.println("The sum of a and b is: " + sum);
+    }
+}
